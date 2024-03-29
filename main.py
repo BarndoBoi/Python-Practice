94058a4edc1b5199d5c96e15892d9f0668aa6d0e
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-03-29 14:05:36 -0400

    Update gitignore and swap to using github api to query my username

--- main.py
+++ main.py
@@ -1,16 +1,36 @@
 import requests
 from pprint import pprint
-import sys
+import sys, os
+import json
 
-response = requests.get('https://github.com/BarndoBoi') #Fetch my github profile
+github_url = 'https://api.github.com'
+username = 'BarndoBoi'
+
+"""Header information here. Not using Github API's version info as we want the latest."""
+accept_header = 'application/vnd.github+json'
+user_agent = 'VickyHub/0.0.1'
+headers = {'user-agent' : user_agent, 'accept' : accept_header}
+
+#Dictionary stores the name of the endpoint, then a tuple with the request type and the endpoint's url
+endpoints = {'User': '/users/' + username}
+
+response = requests.get(github_url + endpoints['User'], headers=headers)
 pprint(response.text)
+
+folder_path = 'data_dumps'
+os.makedirs(folder_path, exist_ok=True)
+file_path = os.path.join(folder_path, 'json_response.json')
+
+if response.json():
+    with open(file_path, 'w') as file:
+        file.write(json.dumps(response.json(), indent=4))
 
 if len(sys.argv) > 1:
     output_to_file = sys.argv[1]
 else:
     output_to_file = None
 
-if output_to_file == "true":
+if output_to_file == 'true':
     #Dump the response text to a file for viewing
-    with open("response.html", "w") as file:
+    with open('response.html', 'w') as file:
         file.write(response.text)
